61c961890f059d96b51109134f50a596d20d4adc
yeonuk44 <40634025+yeonuk44@users.noreply.github.com>
2019-12-04 17:16:35 +0900

    sensor update
    speed :20 
    IR direct : 60cm -> -10 rpm

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ Serial pc(USBTX,USBRX); // Tx, Rx Pin
 //Set each gpio to see the output of the hall sensor as a led
 LaneSensor laneSensor(p11,p12,p13,p14,p15,p16,p17);
 
-    
+
 void counter_1ms ()
 {
     uiFlag_50ms++;
@@ -38,45 +38,45 @@ int main()
 
     Timer time;
     time.start();
-    
+
     //Servo reset
     float degree = 0.0;
-    //pc.printf("Waiting Request\n");        
-    
-    //servo.update (17.2); //1m 반경 
+    //pc.printf("Waiting Request\n");
+
+    //servo.update (17.2); //1m 반경
     while(1) {
         // Every 50 ms,
         if(uiFlag_50ms>=50) {
             uiFlag_50ms=0;
-            
+
            // // clear plotting buffer
 //            plot.reset();
-//            
+//
 //            // put data to buffer
 //            plot.put(pir.getDistance_cm(),0);
-//            
+//
 //            // send buffer
 //            plot.send(&pc);
 //            pc.printf ("Distance %f\r\n", pir.getDistance_cm());
-        }  
-        
+        }
+
         //AEB
         if(pir.getDistance_cm() <= 40)
-            motor.setSpeed_percent(0, BACKWARD);
-        else 
+            motor.setSpeed_percent(-10, BACKWARD);
+        else
             motor.setSpeed_percent(20.0,FORWARD);
-            
+
        servo.update(laneSensor.getError()/1.3);
     }
 }
 
- //Servo control   
+ //Servo control
       //  char c = pc.getc();
-//        
+//
 //        if (c == 'u') degree += 0.1;
 //        else if (c == 'd') degree -= 0.1;
-//            
+//
 //        servo.update (degree);
 //        degree = servo.getDegree();
-//            
+//
 //        pc.printf("Degree: %f\n", servo.getDegree());
